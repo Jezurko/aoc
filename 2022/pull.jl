222d430f388152d7ae4b9b5854529ca144f6a811
Robert Konicar <xkonicar@fi.muni.cz>
2022-12-01 11:53:05 +0100

    2022: Update puller to include user agent.

--- 2022/pull.jl
+++ 2022/pull.jl
@@ -1,9 +1,16 @@
 using DotEnv
 using HTTP
 
-DotEnv.config( path = "../.env" )
-day =  Base.Filesystem.basename( pwd() )
-res = HTTP.get( "http://adventofcode.com/" * ENV["year"] * "/day/" * day * "/input",
-               cookies=Dict("session"=>ENV["session"]) )
-out = open( "input.txt", "w" )
-println( out, String(res.body) )
+function pull()
+    DotEnv.config( path = "../.env" )
+    day =  Base.Filesystem.basename( pwd() )
+    agent = "https://github.com/Jezurko/aoc/tree/main/2022 by " * ENV["mail"]
+    res = HTTP.get( "http://adventofcode.com/" * ENV["year"] * "/day/" * day * "/input",
+                   cookies=Dict( "session"=>ENV["session"] ),
+                   headers=Dict( ["User-agent"=>agent] )
+                   )
+    out = open( "input.txt", "w" )
+    println( out, String(res.body) )
+end
+
+pull()
