d51e9d52f39b294264099820195db6bbf9003b51
Robert Konicar <xkonicar@fi.muni.cz>
2022-12-03 16:22:51 +0100

    2022: Remove trailing newline from inputs.

--- 2022/pull.jl
+++ 2022/pull.jl
@@ -3,14 +3,14 @@ using HTTP
 
 function pull()
     DotEnv.config( path = "../.env" )
-    day =  Base.Filesystem.basename( pwd() )
+    day = Base.Filesystem.basename( pwd() )
     agent = "https://github.com/Jezurko/aoc/tree/main/2022 by " * ENV["mail"]
     res = HTTP.get( "http://adventofcode.com/" * ENV["year"] * "/day/" * day * "/input",
                    cookies=Dict( "session"=>ENV["session"] ),
                    headers=Dict( ["User-agent"=>agent] )
                    )
     out = open( "input.txt", "w" )
-    println( out, String(res.body) )
+    println( out, chomp( String(res.body) ) )
 end
 
 pull()
