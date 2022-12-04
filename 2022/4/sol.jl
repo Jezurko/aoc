f7e7e94af25cd7c89ef4900fdabae5481efae807
Robert Konicar <xkonicar@fi.muni.cz>
2022-12-04 11:09:04 +0100

    2022: Cleanup day 4(thanks Mr. Horse <3).

--- 2022/4/sol.jl
+++ 2022/4/sol.jl
@@ -2,23 +2,18 @@ input = readchomp( "input.txt" )
 
 pairs() = split.( split( input, "\n" ), "," )
 
-makeSets( pair ) = map( x->BitSet( parse(Int, x[begin]) : parse(Int, x[end]) ), split.( pair, "-" ) )
+make_sets( pair ) = map( x->BitSet( parse( Int, x[begin] ) : parse( Int, x[end] ) ),
+                         split.( pair, "-" ) )
 
 function check_pair( pair )
-    Sets = makeSets( pair )
-    Sets[begin] ⊆ Sets[end] || Sets[end] ⊆ Sets[begin]
+    sets = make_sets( pair )
+    sets[begin] ⊆ sets[end] || sets[end] ⊆ sets[begin]
 end
 
-function check_overlap( pair )
-    Sets = makeSets( pair )
-    !isempty( Sets[begin] ∩ Sets[end] )
-end
+check_overlap( pair ) = !isdisjoint( make_sets( pair )... )
 
-count_subsets() = sum( map( check_pair, pairs() ) )
-count_overlaps() = sum( map( check_overlap, pairs() ) )
+count_subsets() = sum( check_pair, pairs() )
+count_overlaps() = sum( check_overlap, pairs() )
+
 println( count_subsets() )
 println( count_overlaps() )
-
-
-
-
