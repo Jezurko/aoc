60d3ed82df752480deddcdceaabbc428186adfe7
Robert Konicar <xkonicar@fi.muni.cz>
2022-12-10 14:27:55 +0100

    2022: Fix folder naming scheme and update pull to accommodate.

--- 2022/pull.jl
+++ 2022/pull.jl
@@ -5,7 +5,7 @@ function pull()
     DotEnv.config( path = "../.env" )
     day = Base.Filesystem.basename( pwd() )
     agent = "https://github.com/Jezurko/aoc/tree/main/2022 by " * ENV["mail"]
-    res = HTTP.get( "http://adventofcode.com/" * ENV["year"] * "/day/" * day * "/input",
+    res = HTTP.get( "http://adventofcode.com/" * ENV["year"] * "/day/" * string( parse( Int, day ) ) * "/input",
                    cookies=Dict( "session"=>ENV["session"] ),
                    headers=Dict( ["User-agent"=>agent] )
                    )
