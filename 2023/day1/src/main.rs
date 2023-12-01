d14e2fe0686c603752c75cfd5116f7eb0ec0a0f5
Robert Konicar <xkonicar@fi.muni.cz>
2023-12-01 10:55:48 +0100

    2023:day1: Fix hypothetical error.

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,29 +1,26 @@
 use std::fs::read_to_string;
 use regex::{Captures, Regex};
 
-fn get_lines(file: &str) -> String {
-    read_to_string(file)
-        .expect("invalid file")
-}
+fn get_lines(file: &str) -> String { read_to_string(file).expect("invalid file") }
 
 fn replace_digits(input: &str) -> String {
     let digits = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine)").unwrap();
-    let replace_digits = |cap: &Captures| -> &str {
+    let apply_replace = |cap: &Captures| -> &str {
         match &cap[0] {
-            "one" => "1e",
-            "two" => "2o",
-            "three" => "3e",
-            "four" => "4r",
-            "five" => "5e",
-            "six" => "6x",
-            "seven" => "7n",
-            "eight" => "8t",
-            "nine" => "9e",
+            "one" => "o1e",
+            "two" => "t2o",
+            "three" => "t3e",
+            "four" => "f4r",
+            "five" => "f5e",
+            "six" => "s6x",
+            "seven" => "s7n",
+            "eight" => "e8t",
+            "nine" => "n9e",
             _ => ""
         }
     };
-    let replaced = digits.replace_all(input, replace_digits);
-    return digits.replace_all(&replaced, replace_digits).to_string();
+    let replaced = digits.replace_all(input, apply_replace);
+    return digits.replace_all(&replaced, apply_replace).to_string();
 }
 
 fn load_ints(input: &str) -> Vec< isize > {
@@ -31,18 +28,14 @@ fn load_ints(input: &str) -> Vec< isize > {
     input
         .lines()
         .map(|line|{
-            let first_digit = re.find(line).unwrap().as_str();
             let rev = line.chars().rev().collect::< String >();
-            let last_digit = re.find(&rev).unwrap().as_str();
-            let digits = [first_digit, last_digit].join("");
-            return digits.parse::< isize >().unwrap()
+            let digits = [re.find(line), re.find(&rev)].map(|x|x.unwrap().as_str()).join("");
+            return digits.parse::< isize >().unwrap();
         })
         .collect()
 }
 
 fn main() {
-   let sum: isize = load_ints(&get_lines("inputs/day1.txt")).iter().sum();
-   println!("{}", sum);
-   let sum2: isize = load_ints(&replace_digits(&get_lines("inputs/day1.txt"))).iter().sum();
-   println!("{}", sum2);
+   println!("{}", load_ints(&get_lines("inputs/day1.txt")).iter().sum::< isize >());
+   println!("{}", load_ints(&replace_digits(&get_lines("inputs/day1.txt"))).iter().sum::< isize >());
 }
