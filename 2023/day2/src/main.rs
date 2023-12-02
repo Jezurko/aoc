9b6e699782944a899f14f79821d10144f0a4afe8
Robert Konicar <xkonicar@fi.muni.cz>
2023-12-02 08:02:21 +0100

    2023:day2: Make regices global constants.

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -1,20 +1,25 @@
 use std::fs::read_to_string;
 use regex::{Regex};
 use std::cmp;
+use lazy_static::lazy_static;
 
 fn get_file(file: &str) -> String { read_to_string(file).expect("invalid file") }
 
 fn get_pulls(game: &str) -> Vec< &str > { game.split(':').collect::< Vec< &str > >()[1].split(';').collect() }
 
 enum Colour {red, green, blue}
+
+lazy_static! { static ref red_re:   Regex = Regex::new(r".* ([0-9]+) red.*").unwrap(); }
+lazy_static! { static ref green_re: Regex = Regex::new(r".* ([0-9]+) green.*").unwrap(); }
+lazy_static! { static ref blue_re:  Regex = Regex::new(r".* ([0-9]+) blue.*").unwrap(); }
+
 fn get_colour(pull: &str, colour: Colour) -> isize
 {
-    let re: Regex = match colour {
-        Colour::red   => Regex::new(r".* ([0-9]+) red.*").unwrap(),
-        Colour::green => Regex::new(r".* ([0-9]+) green.*").unwrap(),
-        Colour::blue  => Regex::new(r".* ([0-9]+) blue.*").unwrap(),
+    let res= match colour {
+        Colour::red   => red_re.captures(pull),
+        Colour::green => green_re.captures(pull),
+        Colour::blue  => blue_re.captures(pull),
     };
-    let res = re.captures(pull);
     let x = match res {
         Some(res) => res.get(1).unwrap().as_str().parse::< isize >().unwrap(),
         None      => 0
