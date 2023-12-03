5566e5f42f7623fb518947f0aa79b1f66b11c5bb
Robert Konicar <xkonicar@fi.muni.cz>
2023-12-03 18:26:13 +0100

    2023:day03: Final refactor.

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -1,20 +1,19 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, collections::HashSet, cmp};
 use regex::{Regex};
 use lazy_static::lazy_static;
-use std::cmp;
-use std::collections::HashSet;
 
 fn get_file(file: &str) -> String { read_to_string(file).expect("invalid file") }
+fn get_str_lines(input: &String) -> Vec< &str > { input.lines().collect() }
 
 lazy_static!{ static ref re:Regex = Regex::new(r"[0-9]+").unwrap(); }
 
-fn insert_surrounding_pos(lines: &Vec< &str >,
+fn surrounding_pos(lines: &Vec< &str >,
                           symbol_pos: usize,
-                          nums: &mut HashSet::< (usize, usize, usize) >,
                           l: usize)
+    -> HashSet::< (usize, usize, usize) >
 {
-    let line = &lines[l];
-    for num in re.find_iter(line) {
+    let mut nums = HashSet::< (usize, usize, usize) >::new();
+    for num in re.find_iter(&lines[l]) {
         let (start, end) = (num.start(), num.end().saturating_sub(1));
         if !num.is_empty() &&
             ((start >= symbol_pos.saturating_sub(1) && start <= symbol_pos + 1)
@@ -23,6 +22,16 @@ fn insert_surrounding_pos(lines: &Vec< &str >,
             nums.insert((l, num.start(), num.end()));
         }
     }
+    return nums;
+}
+
+fn process_symbol(lines: &Vec< &str >, line: usize, col: usize) -> HashSet< (usize, usize, usize) >
+{
+    let mut nums = HashSet::< (usize, usize, usize) >::new();
+    for i in line.saturating_sub(1)..=cmp::min(line + 1, lines.len() - 1) {
+        nums.extend(surrounding_pos(&lines, col, i));
+    }
+    return nums;
 }
 
 fn get_num(line: &str, start: usize, end: usize) -> isize
@@ -30,16 +39,13 @@ fn get_num(line: &str, start: usize, end: usize) -> isize
     line[start..end].parse::< isize >().unwrap_or(0)
 }
 
-fn part1(input: &str) -> isize
+fn part1(lines: Vec< &str >) -> isize
 {
-    let lines: Vec< &str > = input.lines().collect();
     let symbol_re = Regex::new(r"[^0-9\.]").unwrap();
     let mut nums = HashSet::< (usize, usize, usize) >::new();
-    for i in 0..lines.len() {
-        for matched in symbol_re.find_iter(lines[i]) {
-            for j in i.saturating_sub(1)..=cmp::min(i + 1, lines.len() - 1) {
-                insert_surrounding_pos(&lines, matched.start(), &mut nums, j);
-            }
+    for (i, line) in lines.iter().enumerate() {
+        for matched in symbol_re.find_iter(line) {
+            nums.extend(process_symbol(&lines, i, matched.start()));
         }
     }
     return nums.iter()
@@ -47,16 +53,12 @@ fn part1(input: &str) -> isize
                .sum::< isize >();
 }
 
-fn part2(input: &str) -> isize
+fn part2(lines: Vec< &str >) -> isize
 {
     let mut sum = 0;
-    let lines: Vec< &str > = input.lines().collect();
-    for i in 0..lines.len() {
-        for (j, _) in lines[i].match_indices('*'){
-            let mut gear = HashSet::< (usize, usize, usize) >::new();
-            for k in i.saturating_sub(1)..=cmp::min(i + 1, lines.len() - 1) {
-                insert_surrounding_pos(&lines, j, &mut gear, k);
-            }
+    for (i, line) in lines.iter().enumerate() {
+        for (j, _) in line.match_indices('*') {
+            let gear = process_symbol(&lines, i, j);
             if gear.len() == 2 {
                 sum = sum + gear.iter()
                                 .map(|(l, x, y)| get_num(lines[*l], *x, *y))
@@ -69,6 +71,6 @@ fn part2(input: &str) -> isize
 
 fn main()
 {
-    println!("{}", part1(get_file("inputs/day03.txt").as_str()));
-    println!("{}", part2(get_file("inputs/day03.txt").as_str()));
+    println!("{}", part1(get_str_lines(&get_file("inputs/day03.txt"))));
+    println!("{}", part2(get_str_lines(&get_file("inputs/day03.txt"))));
 }
