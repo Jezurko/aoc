854f9d294073a702b9fccb88ce87ed3f2560a2ea
Robert Konicar <xkonicar@fi.muni.cz>
2023-12-04 06:58:05 +0100

    2023:day04: Add solution.

---
+++ 2023/day04/src/main.rs
@@ -0,0 +1,53 @@
+use std::{ fs::read_to_string, collections::{ HashSet, VecDeque } };
+use regex::{Regex};
+use lazy_static::lazy_static;
+
+fn get_file(file: &str) -> String { read_to_string(file).expect("invalid file") }
+fn get_str_lines(input: &String) -> Vec< &str > { input.lines().collect() }
+
+lazy_static!{ static ref re:Regex = Regex::new(r"[0-9]+").unwrap(); }
+
+fn process_card(card: &str) -> usize
+{
+    let sides = card.split(':')
+        .collect::< Vec< &str > >()[1]
+        .split('|')
+        .map(|x| re.find_iter(x))
+        .map(|list| list
+                        .map(|num| num.as_str().parse::< isize >().unwrap())
+                        .collect::< HashSet< isize > >()
+            ).collect::< Vec< HashSet< isize > > >();
+    return sides[0].intersection(&sides[1]).count();
+}
+
+fn part1(cards: Vec< &str >) -> isize
+{
+    cards.iter()
+         .map(|x| process_card(x))
+         .map(|x| if x > 0 { 2_isize.pow(u32::try_from(x).unwrap() - 1) } else { 0 })
+         .sum::< isize >()
+}
+
+fn part2(cards: Vec< &str >) -> isize
+{
+    let mut total = 0;
+    let mut copies_q = VecDeque::< isize >::new();
+    for card in cards {
+        let copies = if copies_q.len() > 0 { copies_q.pop_front().unwrap() } else { 1 };
+        total = total + copies;
+        let won_cards = process_card(card);
+        if won_cards > copies_q.len() {
+            copies_q.resize(won_cards, 1);
+        }
+        for i in 0..won_cards {
+            copies_q[i] = copies_q[i] + copies;
+
+        }
+    }
+    return total;
+}
+
+fn main() {
+    println!("{}",part1(get_str_lines(&get_file("inputs/day04.txt"))));
+    println!("{}",part2(get_str_lines(&get_file("inputs/day04.txt"))));
+}
